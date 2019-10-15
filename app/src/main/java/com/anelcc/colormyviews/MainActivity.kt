5c26d029e6ce86e74dc2f4120385761356418e47
Anel CC <anel.calvo.cervantes@gmail.com>
2019-10-15 02:25:49 -0400

    extra changes

--- app/src/main/java/com/anelcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/anelcc/colormyviews/MainActivity.kt
@@ -12,10 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setListener()
     }
-
 
     private fun setListener() {
         val clickableViews: List<View> =
